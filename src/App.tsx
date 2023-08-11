9ff0a7158986a6efeb9ae01f9326c34e50dd16d6
Administrador <lucasgrigol@gmail.com>
2023-08-10 23:21:29 -0300

    Adicionado componente header

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
 import { useState } from 'react'
+import { Header } from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <main className="h-screen w-screen">
+      <Header />
       
       <h1 className="text-5xl font-bold underline bg-orange-600">
         Hello world!
       </h1>
       
-    </>
+    </main>
   )
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+
+export const Header = () => {
+    return (
+        <header>
+            <div>
+                <img src="/assets/logo" alt="logo" />
+            </div>
+            <nav>
+                <ul>
+                    <li>Sobre</li>
+                    <li>Projetos</li>
+                    <li>Habilidades</li>
+                    <li>Contato</li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
