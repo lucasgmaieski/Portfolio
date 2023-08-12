e9c45c61b5db363d9517dfcdfc42d9f574adebdb
Administrador <lucasgrigol@gmail.com>
2023-08-11 23:28:00 -0300

    estilizando header

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <main className="h-screen w-screen">
+    <main className="h-screen w-screen bg-white dark:bg-stone-900">
       <Header />
       
       <h1 className="text-5xl font-bold underline bg-orange-600">
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,28 @@
+import { MdDarkMode } from "react-icons/md";
+import { FiMenu, FiSun} from "react-icons/fi";
+
 
 export const Header = () => {
+
     return (
-        <header>
+        <header className="flex justify-between max-w-5xl m-auto items-center bg-transparent">
             <div>
                 <img src="/assets/logo" alt="logo" />
             </div>
-            <nav>
-                <ul>
+            <nav className="flex flex-row gap-8 items-center">
+                <ul className="flex gap-8 text-orange-500 font-semibold">
+                    <li>Home</li>
                     <li>Sobre</li>
                     <li>Projetos</li>
                     <li>Habilidades</li>
                     <li>Contato</li>
                 </ul>
+                <div>
+                    <FiSun className="block dark:hidden"/>
+                    <MdDarkMode className="hidden dark:block" />
+                </div>
             </nav>
+            <div className="block sm:hidden"><FiMenu /></div>
         </header>
     );
 }
