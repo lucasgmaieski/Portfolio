edcc3ce8885520cdd436a8cf2443eccc167e454b
Administrador <lucasgrigol@gmail.com>
2023-08-24 23:10:48 -0300

    adicionando lista de projetos e estilização

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,53 +1,56 @@
 import { Link } from "react-router-dom";
+import { Button } from "./Button";
 
 
 export const Projects = () => {
     return (
-        <section className="" id="projects">
+        <section className="mb-10 mx-2 md:mb-20" id="projects">
             <div>
-                <h2>Projetos</h2>
+                <h2 className="text-4xl text-bold m-auto relative w-fit before:content-[''] before:absolute before:w-full before:h-1 before:bg-gradient-to-r from-themecolor-500/0 via-themecolor-500 to-themecolor-500/0 before:-bottom-1.5 before:left-0">Projetos</h2>
             </div>
-            <div>
-                <div>
-                    <div className="max-w-xs">
-                        <img src="./src/assets/amper-foods-preview.png" alt="Preview projeto Amper Foods" />
-                    </div>
-                    <div>
-                        <h3>Amper Foods</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores natus velit, debitis accusantium ipsam excepturi eos nihil ullam laboriosam, perferendis inventore enim. Vero, eius inventore ab quis nihil voluptates consequuntur.</p>
-                        <div>
-                            <Link to={''}>Demo</Link>
-                            <Link to={''}>Código</Link>
+            <div className="flex gap-8 mt-10 px-2 items-center flex-col">
+                <div className="group bg-gradient-to-t from-themecolor-500 from-0% via-themecolor-500/50 via-25% to-themecolor-500/0 to-55%  sm:bg-gradient-to-l">
+                    <div className="flex items-center m-1 p-8 sm:pl-0 gap-10 bg-primarylight dark:bg-primarydark flex-col sm:flex-row max-[400px]:p-4">
+                        <div className="max-w-full sm:max-w-[50%] group-hover:shadow-[rgba(234,_179,_8,_0.5)_0px_-5px_26px] sm:group-hover:shadow-[rgba(234,_179,_8,_0.5)_-5px_0px_26px] group-hover:scale-105 transition duration-500">
+                            <img src="./src/assets/amper-foods-preview.png" alt="Preview projeto Amper Foods" />
+                        </div>
+                        <div className="max-w-full sm:max-w-[50%] h-full flex flex-col justify-between">
+                            <h3 className="text-3xl">Amper Foods</h3>
+                            <p className="my-5">Aplicação web de pedidos e entrega de comida online. Desenvolvida com React, Typescript, Styled-Components e Firebase.</p>
+                            <div className="flex gap-5 justify-center sm:gap-7 sm:justify-start">
+                                <Button url="/curriculo" colored={true} text="Demo" />
+                                <Button url="https://www.linkedin.com/in/lucasgmaieski/" colored={false} text="Código" />
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div>
-                <div>
-                    <div className="max-w-xs">
-                        <img src="./src/assets/amper-foods-preview.png" alt="Preview projeto Amper Foods" />
-                    </div>
-                    <div>
-                        <h3>Amper Foods</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores natus velit, debitis accusantium ipsam excepturi eos nihil ullam laboriosam, perferendis inventore enim. Vero, eius inventore ab quis nihil voluptates consequuntur.</p>
-                        <div>
-                            <Link to={''}>Demo</Link>
-                            <Link to={''}>Código</Link>
+                <div className="group bg-gradient-to-t from-themecolor-500 from-0% via-themecolor-500/50 via-25% to-themecolor-500/0 to-55%  sm:bg-gradient-to-r">
+                    <div className="flex items-center m-1 p-8 sm:pr-0 gap-10 bg-primarylight dark:bg-primarydark flex-col sm:flex-row max-[400px]:p-4">
+                        <div className="max-w-full sm:max-w-[50%] group-hover:shadow-[rgba(234,_179,_8,_0.5)_0px_-5px_26px] sm:group-hover:shadow-[rgba(234,_179,_8,_0.5)_5px_0px_26px] group-hover:scale-105 transition duration-500 sm:order-2">
+                            <img src="./src/assets/portfolio-lucas-preview.png" alt="Preview projeto Amper Foods" />
+                        </div>
+                        <div className="max-w-full sm:max-w-[50%] h-full flex flex-col justify-between">
+                            <h3 className="text-3xl">Amper Foods</h3>
+                            <p className="my-5">Aplicação web de pedidos e entrega de comida online. Desenvolvida com React, Typescript, Styled-Components e Firebase.</p>
+                            <div className="flex gap-5 justify-center sm:gap-7 sm:justify-start">
+                                <Button url="/curriculo" colored={true} text="Demo" />
+                                <Button url="https://www.linkedin.com/in/lucasgmaieski/" colored={false} text="Código" />
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div>
-                <div>
-                    <div className="max-w-xs">
-                        <img src="./src/assets/amper-foods-preview.png" alt="Preview projeto Amper Foods" />
-                    </div>
-                    <div>
-                        <h3>Amper Foods</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores natus velit, debitis accusantium ipsam excepturi eos nihil ullam laboriosam, perferendis inventore enim. Vero, eius inventore ab quis nihil voluptates consequuntur.</p>
-                        <div>
-                            <Link to={''}>Demo</Link>
-                            <Link to={''}>Código</Link>
+                <div className="group bg-gradient-to-t from-themecolor-500 from-0% via-themecolor-500/50 via-25% to-themecolor-500/0 to-55%  sm:bg-gradient-to-l">
+                    <div className="flex items-center m-1 p-8 sm:pl-0 gap-10 bg-primarylight dark:bg-primarydark flex-col sm:flex-row max-[400px]:p-4">
+                        <div className="max-w-full sm:max-w-[50%] group-hover:shadow-[rgba(234,_179,_8,_0.5)_0px_-5px_26px] sm:group-hover:shadow-[rgba(234,_179,_8,_0.5)_-5px_0px_26px] group-hover:scale-105 transition duration-500">
+                            <img src="./src/assets/sayit-preview.png" alt="Preview projeto Amper Foods" />
+                        </div>
+                        <div className="max-w-full sm:max-w-[50%] h-full flex flex-col justify-between">
+                            <h3 className="text-3xl">Amper Foods</h3>
+                            <p className="my-5">Aplicação web de pedidos e entrega de comida online. Desenvolvida com React, Typescript, Styled-Components e Firebase.</p>
+                            <div className="flex gap-5 justify-center sm:gap-7 sm:justify-start">
+                                <Button url="/curriculo" colored={true} text="Demo" />
+                                <Button url="https://www.linkedin.com/in/lucasgmaieski/" colored={false} text="Código" />
+                            </div>
                         </div>
                     </div>
                 </div>
---
+++ src/data/ListSkills.ts
@@ -0,0 +1,63 @@
+export const ListSkills = [
+    {
+        name: 'React',
+        path: '/src/assets/reactjs.svg'
+    },
+    {
+        name: 'Typescript',
+        path: '/src/assets/typescript.png'
+    },
+    {
+        name: 'Next Js',
+        path: '/src/assets/nextjs.svg'
+    },
+    {
+        name: 'Styled-Components',
+        path: '/src/assets/styled-components.svg'
+    },
+    {
+        name: 'Tailwindcss',
+        path: '/src/assets/tailwindcss.svg'
+    },
+    {
+        name: 'Bootstrap',
+        path: '/src/assets/bootstrap.svg'
+    },
+    {
+        name: 'Sass',
+        path: '/src/assets/sass.svg'
+    },
+    {
+        name: 'Firebase',
+        path: '/src/assets/firebase.svg'
+    },
+    {
+        name: 'HTML',
+        path: '/src/assets/html.svg'
+    },
+    {
+        name: 'CSS',
+        path: '/src/assets/css.svg'
+    },
+    {
+        name: 'Javascript',
+        path: '/src/assets/javascript.png'
+    },
+    {
+        name: 'Git',
+        path: '/src/assets/git.svg'
+    },
+    {
+        name: 'WordPress',
+        path: '/src/assets/wordpress.svg'
+    },
+    {
+        name: 'PHP',
+        path: '/src/assets/php.png'
+    },
+    {
+        name: 'Figma',
+        path: '/src/assets/figma.svg'
+    },
+    
+]
